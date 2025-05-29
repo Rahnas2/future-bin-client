57ca307bdb3f2a52168e12a0ad23a51ce83b8622
Rahnas p <rahnasx2@gmail.com>
2025-05-29 12:03:14 +0530

    refactor(vite.config): remove unwanted lines of code

--- vite.config.ts
+++ vite.config.ts
@@ -15,18 +15,7 @@ export default defineConfig({
     },
   },
   server: {
-    // host: true,
-    host: '0.0.0.0', // Bind to all interfaces
-    port: 5173,
-    watch: {
-      usePolling: true, // Fix file watching in Docker
-      // interval: 1000,
-    },
-    hmr: {
-      host: 'localhost', // Browser connects to localhost for HMR
-      port: 5173,
-      protocol: 'ws',
-    },
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
